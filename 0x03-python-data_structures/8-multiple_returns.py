2449e08e12c48052d9510cab699700a1a761e703
Jo Kar <josi.kelechi@gmail.com>
2022-12-03 03:01:23 -0800

    Modify 8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        first_char = None
+        _tuple = (length, first_char)
+        return (_tuple)
+    first_char = sentence[0]
+    _tuple = (length, first_char)
+    return 
