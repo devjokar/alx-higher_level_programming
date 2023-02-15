d4a53775adb9f77074a9108a3d85d9ce860b5888
Jo Kar <josi.kelechi@gmail.com>
2023-02-15 13:35:49 -0800

    Fill the project tasks files

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+SELECT id, name -- Query to list all the cities from California
+FROM cities
+WHERE state_id = ( -- Query to get the id of California
+      SELECT id
+      FROM states
+      WHERE name = "California");
