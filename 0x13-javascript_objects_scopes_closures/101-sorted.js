0ebe79e3d27c42c58b16284ce1f76ffed6402f4e
Jo Kar <josi.kelechi@gmail.com>
2023-03-14 12:01:10 -0700

    Resolve style issues

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,11 +2,11 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.keys(dict).map(function (key, index) {
+Object.keys(dict).map((key, index) => {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [];
   }
-  newDict[dict[key]].push(key);
+  return newDict[dict[key]].push(key);
 });
 
 console.log(newDict);
